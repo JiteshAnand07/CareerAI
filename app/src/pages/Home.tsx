24b7dcbcedeb84c2794b6ac16a9cc1c59e86c390
JiteshAnand07 <jiteshanand2005@gmail.com>
2026-07-11 23:49:22 +0530

    Created Home Page

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,5 +1,17 @@
-const Home = () => {
-  return <div className="min-h-screen mt-20">Home</div>
-}
+import CTABanner from "../components/ctabanner";
+import Features from "../components/features";
+import Hero from "../components/hero";
+import Pricing from "../components/pricing";
 
-export default Home
+const Home = () => {
+  return (
+    <div className="bg-page">
+      <Hero />
+      <Features />
+      <Pricing />
+      <CTABanner />
+    </div>
+  );
+};
+
+export default Home;
